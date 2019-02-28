44f4dd0e1da617674f8500975a65ea36298e236b
AlexKoala <alexkoala@alexkoala.com>
2019-03-01 00:41:55 +0900

    Translate some text in top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -153,7 +153,7 @@ class Home extends Component {
                     </CtaItem>
                     <CtaItem>
                       <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                        튜토리얼 읽어보기
+                        자습서 읽어보기
                       </ButtonLink>
                     </CtaItem>
                   </Flex>
@@ -290,7 +290,7 @@ class Home extends Component {
                 </CtaItem>
                 <CtaItem>
                   <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                    튜토리얼 읽어보기
+                    자습서 읽어보기
                   </ButtonLink>
                 </CtaItem>
               </Flex>
