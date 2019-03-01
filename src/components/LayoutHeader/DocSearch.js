7cf0be3778587dfb472db6460112327e5ee331b7
AlexKoala <alexkoala@alexkoala.com>
2019-03-01 15:32:32 +0900

    Translate search placeholder

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -97,7 +97,7 @@ class DocSearch extends Component<{}, State> {
           }}
           id="algolia-doc-search"
           type="search"
-          placeholder="Search"
+          placeholder="검색"
           aria-label="Search docs"
         />
       </form>
