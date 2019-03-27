13a07042c7d1378853fe1ace5b4b63618b493ae0
Phoebe JH Hong <phoebejhhong@gmail.com>
2019-03-27 22:53:31 +1100

    review comments addressed

--- examples/context/motivation-problem.js
+++ examples/context/motivation-problem.js
@@ -6,10 +6,10 @@ class App extends React.Component {
 
 function Toolbar(props) {
   // highlight-range{1-4,7}
-  // Toolbar 컴포넌트는  ThemedButton에게 보내주기 위해
-  // 불필요한 테마 prop를 받고 있습니다.
+  // Toolbar 컴포넌트는 불필요한 테마 prop를 받아서
+  // ThemeButton에 전달해야 합니다.
   // 앱 안의 모든 버튼이 테마를 알아야 한다면
-  // 이 정보를 일일히 넘기는 과정은 매우 곤혹스러울 수 있습니다.
+  // 이 정보를 일일이 넘기는 과정은 매우 곤혹스러울 수 있습니다.
   return (
     <div>
       <ThemedButton theme={props.theme} />
