00eb0df652c7951295c6382c8e62688845bcec36
Taehwan Noh <taehwanno.dev@gmail.com>
2019-11-05 12:13:39 +0900

    Resolve merge conflict
    Related to c2d0cf2

--- src/site-constants.js
+++ src/site-constants.js
@@ -7,13 +7,8 @@
 
 // NOTE: We can't just use `location.toString()` because when we are rendering
 // the SSR part in node.js we won't have a proper location.
-<<<<<<< HEAD
 const urlRoot = 'https://ko.reactjs.org';
-const version = '16.10.2';
-=======
-const urlRoot = 'https://reactjs.org';
 const version = '16.11.0';
->>>>>>> 3846b501066388c4d0bc84fc8389918d9080d5d2
 const babelURL = 'https://unpkg.com/babel-standalone@6.26.0/babel.min.js';
 
 export {babelURL, urlRoot, version};
