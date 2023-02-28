e25ef6dea109ee966a6bdafa519805c1fcffc618
KyungJi Kim <kkj2551@naver.com>
2023-02-28 21:35:23 +0900

    fix: 오타 수정(작접 -> 직접) (#574)

--- examples/forwarding-refs/fancy-button-simple-ref.js
+++ examples/forwarding-refs/fancy-button-simple-ref.js
@@ -5,6 +5,6 @@ const FancyButton = React.forwardRef((props, ref) => (
   </button>
 ));
 
-// 이제 DOM 버튼으로 ref를 작접 받을 수 있습니다.
+// 이제 DOM 버튼으로 ref를 직접 받을 수 있습니다.
 const ref = React.createRef();
 <FancyButton ref={ref}>Click me!</FancyButton>;
