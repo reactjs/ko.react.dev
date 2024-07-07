5968a68b5054c60ab7d5367f41ccef0084e38613
루밀LuMir <rpfos@naver.com>
2024-07-07 22:13:11 +0900

    refactor: change the structure of textlint/data/rules/translateGlossary.js
    확장성을 위해 데이터 구조 변경.

--- textlint/tests/rules/translateGlossary.spec.js
+++ textlint/tests/rules/translateGlossary.spec.js
@@ -6,24 +6,26 @@ const {errMsgTranslateGlossary} = require('../../utils/errMsg');
 const tester = new TextLintTester();
 
 describe('translateGlossary', function () {
-  Object.values(data).forEach((type) => {
-    type.forEach(({target, meta}) => {
-      tester.run(`term: ${meta.term}`, rule, {
-        invalid: [
-          {
-            text: `한글이 포함된 Str node. ${meta.term} 가나다 abc.`,
-            errors: [
-              {
-                message: errMsgTranslateGlossary(meta.term, target),
-              },
-            ],
-          },
-        ],
-        valid: [
-          `한글이 포함된 Str node. "${meta.term}" 라마바 def.`, // stripDoubleQuotes func should be applied.
-          `한글이 포함된 Str node. (${meta.term}) 사아자 ghi.`, // stripParentheses func should be applied.
-          target,
-        ],
+  Object.values(data).forEach((type1) => {
+    Object.values(type1).forEach((type2) => {
+      type2.forEach(({target, meta}) => {
+        tester.run(`term: ${meta.term}`, rule, {
+          invalid: [
+            {
+              text: `한글이 포함된 Str node. ${meta.term} 가나다 abc.`,
+              errors: [
+                {
+                  message: errMsgTranslateGlossary(meta.term, target),
+                },
+              ],
+            },
+          ],
+          valid: [
+            `한글이 포함된 Str node. "${meta.term}" 라마바 def.`, // stripDoubleQuotes func should be applied.
+            `한글이 포함된 Str node. (${meta.term}) 사아자 ghi.`, // stripParentheses func should be applied.
+            target,
+          ],
+        });
       });
     });
   });
