88cc493e09a66a690429beb7e1bd1c6968e7bdfa
루밀LuMir <rpfos@naver.com>
2024-09-21 15:53:22 +0900

    fix: stdin is not a tty error in husky

---
+++ .husky/common.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Windows 10, Git Bash and Yarn workaround
+if command_exists winpty && test -t 1; then
+  exec < /dev/tty
+fi
