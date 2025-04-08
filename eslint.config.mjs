a54d8880cd5c11440e378fa945710b375cf06240
루밀LuMir <rpfos@naver.com>
2025-04-08 16:08:08 +0900

    chore: introduce `no-curly-quote` and `no-irregular-whitespace` rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,16 @@ export default [
     ...mark.configs.baseGfm,
     files: ['src/content/**/*.md'],
     rules: {
+      'mark/no-curly-quote': [
+        'error',
+        {leftSingleQuotationMark: false, rightSingleQuotationMark: false},
+      ],
       'mark/no-double-space': 'error',
       'mark/no-git-conflict-marker': ['error', {skipCode: false}],
+      'mark/no-irregular-whitespace': [
+        'error',
+        {skipCode: false, skipInlineCode: false},
+      ],
     },
   },
 ];
